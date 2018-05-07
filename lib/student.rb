13a2762216ba2acbf6f28f9e5ae04ceef83c494e
Annie Bourke <aabourke@gmail.com>
2018-05-07 20:22:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,13 @@ class Student
   end
 
   def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade < 12
+    SQL
 
+    DB[:conn].execute(sql)
   end
 
   def self.first_X_students_in_grade_10(students)
